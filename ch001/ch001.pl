6129787a29d4157e60e32210cd78cf37a8bb85a4
umcastello <umcastello@gmail.com>
2022-09-29 10:40:21 -0300

    Update ch001.pl

--- ch001/ch001.pl
+++ ch001/ch001.pl
@@ -1,1 +1,16 @@
+#!/usr/bin/env perl
 
+use strict;
+use warnings;
+use v5.34.0;
+	
+	
+my $s = "Perl Weekly Challenge";
+
+$s =~ s/([e])/\U$1/g;
+
+print qq{$s\n};
+
+my $n = () = $s =~ /e/gi;
+
+print qq{$n\n}

446ce0e9306cf4ba0dd03bd343dfda6efe6afa3f
umcastello <umcastello@gmail.com>
2022-09-29 10:44:31 -0300

    Update ch001.pl

--- ch002/ch001.pl
+++ ch002/ch001.pl
@@ -1,1 +1,16 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use v5.34.0;
+
+my @esc = qw/01 010 0100 01000 0010 000100/;
+
+my $size = @esc;
+my $a = 0;
+
+until ( $a >= $size  ) {
+     printf "%d\n", ($esc[$a]);
+     $a = $a + 1;
+}
 
